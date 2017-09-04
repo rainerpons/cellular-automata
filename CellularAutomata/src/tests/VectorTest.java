4ddd5f85af306b5c38225a0bf3e2e9b126bbddd9
Rainer Pons <rpons@ucsc.edu>
2017-09-04 03:12:40 -0700

    Create test class for Vector
    Create test class VectorTest which tests the methods isValid() and
    initializeVector() from the Vector class.

---
+++ CellularAutomata/src/tests/VectorTest.java
@@ -0,0 +1,34 @@
+
+package tests;
+
+import org.junit.Test;
+
+/*
+ * VectorTest.java
+ */
+
+public class VectorTest {
+	// assert true if vector is valid
+	@Test
+	public void testIsValidPositive() {
+		// testIsValidPositive() code
+	}
+
+	// assert false if vector is invalid
+	@Test
+	public void testIsValidNegative() {
+		// testIsValidNegative() code
+	}
+
+	// assert true if vector is valid
+	@Test
+	public void testInitializeVectorPositive() {
+		// testInitializeVectorPositive() code
+	}
+
+	// assert false if vector is invalid
+	@Test
+	public void testInitializeVectorNegative() {
+		// testInitializeVectorNegative() code
+	}
+}
