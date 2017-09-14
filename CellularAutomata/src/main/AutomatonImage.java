d56f7bbc47ba54acff158e19ebd652e297aa91f5
Rainer Pons <rpons@ucsc.edu>
2017-09-13 18:17:35 -0700

    Add save feature functionality
    Add functionality for the save button. Can save the display image to
    user's desktop without hard-coding any directories.

--- CellularAutomata/src/main/AutomatonImage.java
+++ CellularAutomata/src/main/AutomatonImage.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import javax.swing.JPanel;
 
 /*
- * AutomatonImage.java 
+ * AutomatonImage.java
  */
 
 @SuppressWarnings("serial") public class AutomatonImage extends JPanel {
