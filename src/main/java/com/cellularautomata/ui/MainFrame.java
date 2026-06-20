675acba8623a342cb120974277b697f0e228c01b
rainerpons <rainerpons11@gmail.com>
2026-06-20 03:35:16 -0700

    Modernize UI layout and theme

--- src/main/java/com/cellularautomata/ui/MainFrame.java
+++ src/main/java/com/cellularautomata/ui/MainFrame.java
@@ -5,12 +5,11 @@ import com.cellularautomata.engine.AutomataResult;
 import com.cellularautomata.engine.RuleValidator;
 import com.cellularautomata.engine.Vector;
 import com.cellularautomata.image.AutomatonImage;
+import com.formdev.flatlaf.FlatDarkLaf;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -26,7 +25,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileSystemView;
 
 /**
@@ -42,19 +42,19 @@ public class MainFrame {
   /** Initializes every component of the GUI. */
   public MainFrame() {
     try {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      FlatDarkLaf.setup();
     } catch (Exception e) {
-      System.err.println("Could not set native look and feel.");
+      System.err.println("Could not set FlatDarkLaf look and feel.");
     }
 
     // creates the cellular automata frame
     caFrame = new JFrame();
     caFrame.setResizable(false);
     caFrame.setTitle("Cellular Automata");
-    caFrame.setBounds(100, 100, 640, 360);
+    caFrame.setBounds(100, 100, 800, 500);
     caFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     GridBagLayout gridBagLayout = new GridBagLayout();
-    gridBagLayout.columnWidths = new int[] {358, 0, 0};
+    gridBagLayout.columnWidths = new int[] {450, 0, 0};
     gridBagLayout.rowHeights = new int[] {229, 0, 0};
     gridBagLayout.columnWeights = new double[] {1.0, 1.0, Double.MIN_VALUE};
     gridBagLayout.rowWeights = new double[] {1.0, 1.0, Double.MIN_VALUE};
@@ -63,20 +63,24 @@ public class MainFrame {
     // creates the display panel
     JPanel displayPanel = new JPanel();
     displayPanel.setLayout(new BorderLayout());
-    displayPanel.setBorder(BorderFactory.createTitledBorder("Display"));
     displayPanel.setToolTipText("");
     GridBagConstraints gbcDisplayPanel = new GridBagConstraints();
     gbcDisplayPanel.gridheight = 2;
-    gbcDisplayPanel.insets = new Insets(10, 10, 10, 5);
+    gbcDisplayPanel.insets = new Insets(20, 20, 20, 10);
     gbcDisplayPanel.fill = GridBagConstraints.BOTH;
     gbcDisplayPanel.gridx = 0;
     gbcDisplayPanel.gridy = 0;
     caFrame.getContentPane().add(displayPanel, gbcDisplayPanel);
 
+    JLabel displayHeading = new JLabel("Display");
+    displayHeading.putClientProperty("FlatLaf.styleClass", "h3");
+    displayHeading.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+    displayPanel.add(displayHeading, BorderLayout.NORTH);
+
     // creates the automaton label in display panel
     final JLabel automatonLabel =
         new JLabel(
-            "<html><center>Click Display to generate an automaton.<br><br>"
+            "<html><center>Click Generate Automaton to begin.<br><br>"
                 + "<font color='gray'>Adjust size, rule, or seed type first.</font>"
                 + "</center></html>");
     automatonLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -84,72 +88,60 @@ public class MainFrame {
 
     // creates the parameters panel
     JPanel parametersPanel = new JPanel();
-    parametersPanel.setBorder(BorderFactory.createTitledBorder("Parameters"));
     GridBagConstraints gbcParametersPanel = new GridBagConstraints();
-    gbcParametersPanel.insets = new Insets(10, 5, 5, 10);
+    gbcParametersPanel.insets = new Insets(20, 10, 5, 20);
     gbcParametersPanel.fill = GridBagConstraints.BOTH;
     gbcParametersPanel.gridx = 1;
     gbcParametersPanel.gridy = 0;
     caFrame.getContentPane().add(parametersPanel, gbcParametersPanel);
     GridBagLayout gblParametersPanel = new GridBagLayout();
-    gblParametersPanel.columnWidths = new int[] {103, 0, 0};
-    gblParametersPanel.rowHeights = new int[] {31, 45, 48, 50, 0, 0};
+    gblParametersPanel.columnWidths = new int[] {120, 0, 0};
+    gblParametersPanel.rowHeights = new int[] {30, 40, 50, 50, 50, 0};
     gblParametersPanel.columnWeights = new double[] {0.0, 1.0, Double.MIN_VALUE};
-    gblParametersPanel.rowWeights = new double[] {1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+    gblParametersPanel.rowWeights = new double[] {0.0, 0.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
     parametersPanel.setLayout(gblParametersPanel);
 
+    JLabel parametersHeading = new JLabel("Parameters");
+    parametersHeading.putClientProperty("FlatLaf.styleClass", "h3");
+    GridBagConstraints gbcParametersHeading = new GridBagConstraints();
+    gbcParametersHeading.gridwidth = 2;
+    gbcParametersHeading.anchor = GridBagConstraints.WEST;
+    gbcParametersHeading.insets = new Insets(0, 0, 15, 0);
+    gbcParametersHeading.gridx = 0;
+    gbcParametersHeading.gridy = 0;
+    parametersPanel.add(parametersHeading, gbcParametersHeading);
+
     // creates the size label in parameters panel
+    final JLabel sizeLabel = new JLabel("Size (8)");
     GridBagConstraints gbcSizeLabel = new GridBagConstraints();
     gbcSizeLabel.gridwidth = 2;
+    gbcSizeLabel.anchor = GridBagConstraints.WEST;
     gbcSizeLabel.insets = new Insets(0, 0, 5, 0);
     gbcSizeLabel.gridx = 0;
-    gbcSizeLabel.gridy = 0;
-    JLabel sizeLabel = new JLabel("Size");
+    gbcSizeLabel.gridy = 1;
     parametersPanel.add(sizeLabel, gbcSizeLabel);
-
-    // creates the current size label in parameters panel
-    GridBagConstraints gbcCurrentSizeLabel = new GridBagConstraints();
-    gbcCurrentSizeLabel.insets = new Insets(0, 0, 5, 5);
-    gbcCurrentSizeLabel.gridx = 0;
-    gbcCurrentSizeLabel.gridy = 2;
-    JLabel currentSizeLabel = new JLabel("Current size");
-    parametersPanel.add(currentSizeLabel, gbcCurrentSizeLabel);
-
-    // creates the current size text field in parameters panel
-    final JTextField currentSizeTextField = new JTextField();
-    currentSizeTextField.setText(Integer.toString(8));
-    currentSizeTextField.setEditable(false);
-    GridBagConstraints gbcCurrentSizeTextField = new GridBagConstraints();
-    gbcCurrentSizeTextField.insets = new Insets(0, 0, 5, 0);
-    gbcCurrentSizeTextField.fill = GridBagConstraints.HORIZONTAL;
-    gbcCurrentSizeTextField.gridx = 1;
-    gbcCurrentSizeTextField.gridy = 2;
-    parametersPanel.add(currentSizeTextField, gbcCurrentSizeTextField);
 
     // creates the size slider in parameters panel
     final JSlider sizeSlider = new JSlider(JSlider.HORIZONTAL, 4, 64, 8);
-    sizeSlider.addMouseMotionListener(
-        new MouseMotionAdapter() {
+    sizeSlider.addChangeListener(
+        new ChangeListener() {
           @Override
-          public void mouseDragged(MouseEvent e) {
-            currentSizeTextField.setText(Integer.toString(sizeSlider.getValue()));
+          public void stateChanged(ChangeEvent e) {
+            sizeLabel.setText("Size (" + sizeSlider.getValue() + ")");
           }
         });
     GridBagConstraints gbcSizeSlider = new GridBagConstraints();
     gbcSizeSlider.fill = GridBagConstraints.HORIZONTAL;
     gbcSizeSlider.gridwidth = 2;
-    gbcSizeSlider.insets = new Insets(0, 0, 5, 0);
+    gbcSizeSlider.insets = new Insets(0, 0, 15, 0);
     gbcSizeSlider.gridx = 0;
-    gbcSizeSlider.gridy = 1;
+    gbcSizeSlider.gridy = 2;
     parametersPanel.add(sizeSlider, gbcSizeSlider);
-    sizeSlider.setMinorTickSpacing(1);
-    sizeSlider.setMajorTickSpacing(5);
-    sizeSlider.setPaintTicks(true);
-    sizeSlider.setSnapToTicks(true);
 
     // creates the rule label in parameters panel
     GridBagConstraints gbcRuleLabel = new GridBagConstraints();
-    gbcRuleLabel.insets = new Insets(0, 0, 5, 5);
+    gbcRuleLabel.anchor = GridBagConstraints.WEST;
+    gbcRuleLabel.insets = new Insets(0, 0, 15, 10);
     gbcRuleLabel.gridx = 0;
     gbcRuleLabel.gridy = 3;
     JLabel ruleLabel = new JLabel("Rule number");
@@ -159,7 +151,7 @@ public class MainFrame {
     final JTextField ruleTextArea = new JTextField();
     ruleTextArea.setText("30");
     GridBagConstraints gbcRuleTextArea = new GridBagConstraints();
-    gbcRuleTextArea.insets = new Insets(0, 0, 5, 0);
+    gbcRuleTextArea.insets = new Insets(0, 0, 15, 0);
     gbcRuleTextArea.fill = GridBagConstraints.HORIZONTAL;
     gbcRuleTextArea.gridx = 1;
     gbcRuleTextArea.gridy = 3;
@@ -167,7 +159,8 @@ public class MainFrame {
 
     // creates the seed label in parameters panel
     GridBagConstraints gbcSeedLabel = new GridBagConstraints();
-    gbcSeedLabel.insets = new Insets(0, 0, 0, 5);
+    gbcSeedLabel.anchor = GridBagConstraints.WEST;
+    gbcSeedLabel.insets = new Insets(0, 0, 0, 10);
     gbcSeedLabel.gridx = 0;
     gbcSeedLabel.gridy = 4;
     JLabel seedLabel = new JLabel("Seed type");
@@ -184,29 +177,35 @@ public class MainFrame {
 
     // creates the commands panel
     JPanel commandsPanel = new JPanel();
-    commandsPanel.setBorder(BorderFactory.createTitledBorder("Commands"));
     GridBagConstraints gbcCommandsPanel = new GridBagConstraints();
-    gbcCommandsPanel.insets = new Insets(5, 5, 10, 10);
+    gbcCommandsPanel.insets = new Insets(5, 10, 20, 20);
     gbcCommandsPanel.fill = GridBagConstraints.BOTH;
     gbcCommandsPanel.gridx = 1;
     gbcCommandsPanel.gridy = 1;
     caFrame.getContentPane().add(commandsPanel, gbcCommandsPanel);
     GridBagLayout gblCommandsPanel = new GridBagLayout();
     gblCommandsPanel.columnWidths = new int[] {0, 0, 0};
-    gblCommandsPanel.rowHeights = new int[] {0, 0};
+    gblCommandsPanel.rowHeights = new int[] {30, 0, 0};
     gblCommandsPanel.columnWeights = new double[] {1.0, 1.0, Double.MIN_VALUE};
-    gblCommandsPanel.rowWeights = new double[] {1.0, Double.MIN_VALUE};
+    gblCommandsPanel.rowWeights = new double[] {0.0, 1.0, Double.MIN_VALUE};
     commandsPanel.setLayout(gblCommandsPanel);
 
+    JLabel commandsHeading = new JLabel("Commands");
+    commandsHeading.putClientProperty("FlatLaf.styleClass", "h3");
+    GridBagConstraints gbcCommandsHeading = new GridBagConstraints();
+    gbcCommandsHeading.gridwidth = 2;
+    gbcCommandsHeading.anchor = GridBagConstraints.WEST;
+    gbcCommandsHeading.insets = new Insets(0, 0, 10, 0);
+    gbcCommandsHeading.gridx = 0;
+    gbcCommandsHeading.gridy = 0;
+    commandsPanel.add(commandsHeading, gbcCommandsHeading);
+
     // creates the save button in commands panel
-    final JButton saveButton = new JButton("Save");
-    // TODO: Centralize UI state management once presentation and application
-    // concerns are
-    // separated.
+    final JButton saveButton = new JButton("Save Image");
     saveButton.setEnabled(false);
 
     // creates the display button in commands panel
-    JButton displayButton = new JButton("Display");
+    JButton displayButton = new JButton("Generate Automaton");
     displayButton.addActionListener(
         e -> {
           String ruleText = ruleTextArea.getText();
@@ -219,7 +218,8 @@ public class MainFrame {
             AutomataResult result = AutomataEngine.generate(rule, size, seedType);
             seed = result.getOriginalSeed(); // retain seed for filename creation during save
             BufferedImage automatonImage = AutomatonImage.getImageFromMap(result.getAutomatonMap());
-            resizedAutomatonImage = AutomatonImage.resizeImage(280, 280, automatonImage);
+            // Empirically scaled from 280x280 to 400x400 for an 800x500 window
+            resizedAutomatonImage = AutomatonImage.resizeImage(400, 400, automatonImage);
             automatonLabel.setText(null);
             automatonLabel.setIcon(new ImageIcon(resizedAutomatonImage));
             saveButton.setEnabled(true);
@@ -234,7 +234,7 @@ public class MainFrame {
     GridBagConstraints gbcDisplayButton = new GridBagConstraints();
     gbcDisplayButton.insets = new Insets(0, 0, 0, 5);
     gbcDisplayButton.gridx = 0;
-    gbcDisplayButton.gridy = 0;
+    gbcDisplayButton.gridy = 1;
     commandsPanel.add(displayButton, gbcDisplayButton);
 
     saveButton.addActionListener(
@@ -255,7 +255,7 @@ public class MainFrame {
         });
     GridBagConstraints gbcSaveButton = new GridBagConstraints();
     gbcSaveButton.gridx = 1;
-    gbcSaveButton.gridy = 0;
+    gbcSaveButton.gridy = 1;
     commandsPanel.add(saveButton, gbcSaveButton);
   }
 
