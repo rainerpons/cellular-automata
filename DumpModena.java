985d324bbaae7fd590cef6dac641399b23b91335
rainerpons <rainerpons11@gmail.com>
2026-07-07 16:01:06 -0700

    Remove test scripts

--- DumpModena.java
+++
@@ -1,9 +0,0 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
-public class DumpModena extends Application {
-    public void start(Stage s) {
-        System.out.println(Application.getUserAgentStylesheet());
-        System.exit(0);
-    }
-    public static void main(String[] args) { launch(args); }
-}
--- test_modena.java
+++
@@ -1,6 +0,0 @@
-import javafx.application.Application;
-public class test_modena {
-    public static void main(String[] args) {
-        System.out.println(Application.getUserAgentStylesheet());
-    }
-}
