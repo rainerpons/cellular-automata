43ed9de1bc6d9c555e5694f55e065011b2332c43
rainerpons <rainerpons11@gmail.com>
2026-07-09 14:27:19 -0700

    Update default Totalistic rule to 5

--- src/main/java/com/cellularautomata/engine/TotalisticRule.java
+++ src/main/java/com/cellularautomata/engine/TotalisticRule.java
@@ -5,7 +5,7 @@ public final class TotalisticRule implements Rule {
 
   public static final int MIN_RULE_NUMBER = 0;
   public static final int MAX_RULE_NUMBER = 15;
-  public static final int DEFAULT_RULE_NUMBER = 7;
+  public static final int DEFAULT_RULE_NUMBER = 5;
   private static final int RULE_WIDTH = 4;
 
   private final int ruleNumber;
